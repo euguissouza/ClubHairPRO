7bdd9a8f3a65391743f4179a1baa3d223c752ac4
euguissouza <contato.guisilva4@hotmail.com>
2026-03-12 00:28:51 -0300

    first-commit

---
+++ ClubHairPRO-API/src/main/java/com/api/clubhairproapi/ClubHairProApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.clubhairproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubHairProApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubHairProApiApplication.class, args);
+    }
+
+}

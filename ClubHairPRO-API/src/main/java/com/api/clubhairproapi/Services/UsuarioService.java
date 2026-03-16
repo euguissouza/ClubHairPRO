7ffc7c70af179842ec5d3301d5ce794e87b3fa14
euguissouza <contato.guisilva4@hotmail.com>
2026-03-15 23:12:50 -0300

    v:0.0.7 - feat-Services-tipo:Usuario

---
+++ ClubHairPRO-API/src/main/java/com/api/clubhairproapi/Services/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.api.clubhairproapi.Services;
+
+import com.api.clubhairproapi.DTO.UsuarioDTO;
+import com.api.clubhairproapi.Entities.Usuario;
+import com.api.clubhairproapi.Mappers.UsuarioMapper;
+import com.api.clubhairproapi.Repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    private UsuarioRepository repository;
+    private UsuarioMapper mapper;
+
+    public UsuarioService(UsuarioRepository repository, UsuarioMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public Usuario salvaNovoUsuario(UsuarioDTO dto){
+        Usuario user = mapper.toEntity(dto);
+        return repository.save(user);
+    }
+
+    public void buscaBanco(){
+        List<Usuario> find = repository.findAll();
+    }
+
+}

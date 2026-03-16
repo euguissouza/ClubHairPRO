e9f454d4bc513ae07c7ed57caec835cac403bc0f
euguissouza <contato.guisilva4@hotmail.com>
2026-03-15 23:12:12 -0300

    v:0.0.5 - feat-Services-tipo:Agendamento

---
+++ ClubHairPRO-API/src/main/java/com/api/clubhairproapi/Services/AgendamentoService.java
@@ -0,0 +1,26 @@
+package com.api.clubhairproapi.Services;
+
+import com.api.clubhairproapi.DTO.AgendamentoDTO;
+import com.api.clubhairproapi.Entities.Agendamento;
+import com.api.clubhairproapi.Mappers.AgendamentoMapper;
+import com.api.clubhairproapi.Repositories.AgendamentoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+    private AgendamentoRepository repository;
+    private AgendamentoMapper mapper;
+
+    public AgendamentoService(AgendamentoRepository repository, AgendamentoMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public Agendamento novoAgendamento(AgendamentoDTO dto){
+        Agendamento agendamento = mapper.toEntity(dto);
+        return repository.save(agendamento);
+    }
+
+
+
+}

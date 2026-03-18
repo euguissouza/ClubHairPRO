78ede62ae518d7e56d62ec0b5a673dfafdf16d30
euguissouza <contato.guisilva4@hotmail.com>
2026-03-18 03:34:50 -0300

    V:0.0.12 - fix-MapperProfissional: toDTO removed

--- ClubHairPRO-API/src/main/java/com/api/clubhairproapi/Mappers/ProfissionalMapper.java
+++ ClubHairPRO-API/src/main/java/com/api/clubhairproapi/Mappers/ProfissionalMapper.java
@@ -11,7 +11,6 @@ public interface ProfissionalMapper {
     @Mapping(target = "id", ignore = true)
     Profissional toEntity(ProfissionalDTO dto);
 
-    @Mapping(target = "nome", ignore = false)
-    ProfissionalDTO toDTO(Profissional profissional);
+
 
 }
